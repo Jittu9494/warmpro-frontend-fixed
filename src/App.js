e851ea6048914d8bc22ac5c1ccb383c8bd05aba6
Jittu9494 <jittu9494@gmail.com>
2025-06-19 12:02:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
-import JobList from "./components/JobList";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/logo.png" alt="WarmPro Logo" className="logo" />
-        <h1>WarmPro Installer App</h1>
-      </header>
-      <main>
-        <JobList />
-      </main>
+    <div style={{ padding: 20 }}>
+      <h1>✅ React is working!</h1>
+      <p>If you can see this, rendering is fine.</p>
     </div>
   );
 }
