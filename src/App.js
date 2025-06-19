74b7ce1cecf3831c1c6ff3d85c93d58313347ef4
Jittu9494 <jittu9494@gmail.com>
2025-06-19 11:56:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
-// Marked for deletion
+import React from "react";
+import JobList from "./components/JobList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.png" alt="WarmPro Logo" className="logo" />
+        <h1>WarmPro Installer App</h1>
+      </header>
+      <main>
+        <JobList />
+      </main>
+    </div>
+  );
+}
+
+export default App;
